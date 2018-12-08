a22a88579747f1e95f97a98caf467610a32b462b
Mtnman <billman.designs@gmail.com>
2018-12-08 15:56:38 -0500

    finally got the dumb stuff working now it's time to get the other dumb stuff working as well.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,49 +4,58 @@ Brett Billman
 December 6th 2018
 */
 
-let train = "";
-let destination = "";
-let firTrain = "";
-let freq = "";
-
+// Initialize Firebase
 $(document).ready(function(){
 
-// Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyCSj4VozDNVAh0ZVay2XS70vzFGwh3rVbc",
-    authDomain: "train-app-data.firebaseapp.com",
-    databaseURL: "https://train-app-data.firebaseio.com",
-    projectId: "train-app-data",
-    storageBucket: "train-app-data.appspot.com",
-    messagingSenderId: "999241241117"
-  };
-  firebase.initializeApp(config);
+    var config = {
+        apiKey: "AIzaSyCSj4VozDNVAh0ZVay2XS70vzFGwh3rVbc",
+        authDomain: "train-app-data.firebaseapp.com",
+        databaseURL: "https://train-app-data.firebaseio.com",
+        projectId: "train-app-data",
+        storageBucket: "train-app-data.appspot.com",
+        messagingSenderId: "999241241117"
+    };
+    firebase.initializeApp(config);
 
-  var database = firebase.database();
+    let database = firebase.database();
+
+    let train = "";
+    let destination = "";
+    let firTrain = "";
+    let freq = "";
 
 
-  $(".button").on("click", function(){
-    event.preventDefault();
+    $("#new-train").on("click", function(event){
+        event.preventDefault();
 
-    train = $("#name").val().trim();
-    destination = $("#destination").val().trim();
-    firTrain = $("#fir-train").val().trim();
-    freq = $("#freq").val().trim();
+        train = $("#name").val().trim();
+        destination = $("#destination").val().trim();
+        firTrain = $("#fir-train").val().trim();
+        freq = $("#freq").val().trim();
+    
+        database.ref().push({
+            train: train,
+            destination: destination,
+            firTrain: firTrain,
+            freq: freq
+        });
 
-    console.log(train);
-    console.log(destination);
-    console.log(firTrain);
-    console.log(freq);
-
-    database.ref().push({
-        train: train,
-        destination: destination,
-        firTrain: firTrain,
-        freq = freq
     });
 
+    database.ref().on("child_added", function(snapshot) {
+        // storing the snapshot.val() in a variable for convenience
+        var sv = snapshot.val();
 
-  });
+        // Console.loging the last user's data
+        console.log(sv.train);
+        console.log(sv.destination);
+        console.log(sv.firTrain);
+        console.log(sv.freq);
 
+        // Handle the errors
+    }, function(errorObject) {
+    console.log("Errors handled: " + errorObject.code);
+    });
 
 });
+
